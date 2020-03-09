b5cb58fff7f1d1a22d4b3bedbaa28514cfde8055
Georgios Pilitsoglou <george.pilitsoglou@gmail.com>
2020-03-09 19:20:54 +0000

    added pokemon search component

--- src/app/pokemon/pokemon.module.ts
+++ src/app/pokemon/pokemon.module.ts
@@ -1,11 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ReactiveFormsModule } from '@angular/forms';
+import { PokemonDetailComponent, PokemonSearchComponent } from '@angular-ngrx/pokemon/components';
 
-export const COMPONENTS = [];
+export const COMPONENTS = [PokemonDetailComponent, PokemonSearchComponent];
+
+export const CONTAINERS = [];
+
 
 @NgModule({
   imports: [CommonModule, ReactiveFormsModule],
-  declarations: COMPONENTS,
+  declarations: [COMPONENTS, CONTAINERS],
 })
 export class PokemonModule {}
