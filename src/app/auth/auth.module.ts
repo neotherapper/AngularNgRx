2287487ff82172217e99c5f555fd4ee57f31d6be
Georgios Pilitsoglou <george.pilitsoglou@gmail.com>
2020-03-09 16:39:32 +0000

    created auth module

---
+++ src/app/auth/auth.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule } from '@angular/forms';
+
+
+export const COMPONENTS = [
+];
+
+@NgModule({
+  imports: [
+    CommonModule,
+    ReactiveFormsModule,
+  ],
+  declarations: COMPONENTS,
+})
+export class AuthModule {}
