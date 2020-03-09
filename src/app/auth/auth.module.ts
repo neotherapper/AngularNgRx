5c7315eec5e95985ce7d58808bc162a88226795f
Georgios Pilitsoglou <george.pilitsoglou@gmail.com>
2020-03-09 16:58:09 +0000

    created login-form component

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -1,14 +1,18 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ReactiveFormsModule } from '@angular/forms';
+import { LoginFormComponent } from './components';
+import { MaterialModule } from '@angular-ngrx/material';
 
 
 export const COMPONENTS = [
+  LoginFormComponent
 ];
 
 @NgModule({
   imports: [
     CommonModule,
+    MaterialModule,
     ReactiveFormsModule,
   ],
   declarations: COMPONENTS,
