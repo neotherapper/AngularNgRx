b761dabb806cc26accc695f036dc06afc038bc43
Georgios Pilitsoglou <george.pilitsoglou@gmail.com>
2020-03-09 18:06:06 +0000

    added find pokemon page actions

--- src/app/pokemon/actions/index.ts
+++ src/app/pokemon/actions/index.ts
@@ -2,10 +2,12 @@ import * as PokemonApiActions from './pokemon-api.actions';
 import * as PokemonActions from './pokemon.actions';
 import * as PokemonCollectionApiActions from './pokemon-collection-api.actions';
 import * as PokemonCollectionPageActions from './pokemon-collection-page.action';
+import * as FindPokemonPageActions from './find-pokemon-page.actions';
 
 export {
   PokemonApiActions,
   PokemonActions,
   PokemonCollectionApiActions,
   PokemonCollectionPageActions,
+  FindPokemonPageActions,
 };
