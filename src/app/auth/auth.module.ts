5e521b15049d0ce2586cc35f1d112c5852776e6a
Georgios Pilitsoglou <george.pilitsoglou@gmail.com>
2020-03-09 17:04:41 +0000

    created auth-routing module

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -3,6 +3,7 @@ import { CommonModule } from '@angular/common';
 import { ReactiveFormsModule } from '@angular/forms';
 import { LoginFormComponent } from './components';
 import { MaterialModule } from '@angular-ngrx/material';
+import { AuthRoutingModule } from '@angular-ngrx/auth/auth-routing.module';
 
 
 export const COMPONENTS = [
@@ -13,6 +14,7 @@ export const COMPONENTS = [
   imports: [
     CommonModule,
     MaterialModule,
+    AuthRoutingModule,
     ReactiveFormsModule,
   ],
   declarations: COMPONENTS,
