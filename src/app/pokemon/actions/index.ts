edbd754a5ba26f84fed4f391d483b50f7907dea6
Georgios Pilitsoglou <george.pilitsoglou@gmail.com>
2020-03-09 18:00:35 +0000

    created pokemon collection page actions

--- src/app/pokemon/actions/index.ts
+++ src/app/pokemon/actions/index.ts
@@ -1,5 +1,11 @@
 import * as PokemonApiActions from './pokemon-api.actions';
 import * as PokemonActions from './pokemon.actions';
 import * as PokemonCollectionApiActions from './pokemon-collection-api.actions';
+import * as PokemonCollectionPageActions from './pokemon-collection-page.action';
 
-export { PokemonApiActions, PokemonActions, PokemonCollectionApiActions };
+export {
+  PokemonApiActions,
+  PokemonActions,
+  PokemonCollectionApiActions,
+  PokemonCollectionPageActions,
+};
