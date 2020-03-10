17aa0917d714d1a61280c3625fc62b34df6ec85b
Georgios Pilitsoglou <george.pilitsoglou@gmail.com>
2020-03-10 00:07:37 +0000

    added more functions at pokemon api

--- src/app/core/services/pokemon-api.service.ts
+++ src/app/core/services/pokemon-api.service.ts
@@ -1,10 +1,10 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
-import { Observable } from 'rxjs';
+import { Observable, of, from } from 'rxjs';
 import { map } from 'rxjs/operators';
 
-import { Pokemon } from '@angular-ngrx/pokemon/models';
+import { Pokemon, PokemonShort } from '@angular-ngrx/pokemon/models';
 
 @Injectable({
   providedIn: 'root',
@@ -15,9 +15,44 @@ export class PokemonApiService {
   constructor(private http: HttpClient) {}
 
   searchPokemon(queryTitle: string): Observable<Pokemon[]> {
+    return from(this.getFirstPokemonFullData());
+  }
+
+  searchPokemons(queryTitle: string): Observable<Pokemon[]> {
     return this.http
-      .get<Pokemon[]>(`${this.API_PATH}?limit=81&q=${queryTitle}`)
-      .pipe(map(pokemon => pokemon || []));
+      .get<{
+        previous: number;
+        next: string;
+        count: number;
+        results: Pokemon[];
+      }>(`${this.API_PATH}?limit=5`)
+      .pipe(map(pokemon => pokemon.results || []));
+  }
+
+  getFirstPokemon(): Promise<PokemonShort[]> {
+    return this.http
+      .get<{
+        previous: number;
+        next: string;
+        count: number;
+        results: PokemonShort[];
+      }>(`${this.API_PATH}?limit=3`)
+      .pipe(map(pokemon => pokemon.results || []))
+      .toPromise();
+  }
+
+  async getFirstPokemonFullData(): Promise<Pokemon[]> {
+    const firstPokemon = await this.getFirstPokemon();
+    let pokemonData: Pokemon[] = [];
+    const pokemonUrlsToSearch = firstPokemon.map(pokemon => pokemon.url);
+
+    return Promise.all(
+      pokemonUrlsToSearch.map(url => this.retrievePokemonByUrl(url))
+    ).then(data => (pokemonData = data));
+  }
+
+  retrievePokemonByUrl(url: string): Promise<Pokemon> {
+    return this.http.get<Pokemon>(url).toPromise();
   }
 
   retrievePokemon(id: string): Observable<Pokemon> {
